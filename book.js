8b52a8470ec5435016989674ea82dfd99828ff74
titanarbba <155007024+titanarbba@users.noreply.github.com>
2024-04-26 16:24:07 +0100

    Update book.js

--- book.js
+++ book.js
@@ -1,1 +1,16 @@
+"const ws = require('ws')
+const w = new ws('wss://api-pub.bitfinex.com/ws/2')
+
+w.on('open', () => w.send(msg))
+
+
+w.on('message', (msg) => console.log(msg.toString()))
+
+let msg = JSON.stringify({ 
+  event: 'subscribe', 
+  channel: 'book', 
+  prec: 'R0',
+  symbol: 'tBTCUSD',
+  
+})"
 
